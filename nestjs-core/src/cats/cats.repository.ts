b25239feefdcb308fb71c5bbaab2b2e975aede2c
isntkyu <jkjkjk5710@gmail.com>
2022-05-02 19:24:38 +0900

    cat repository

---
+++ nestjs-core/src/cats/cats.repository.ts
@@ -0,0 +1,19 @@
+import { CatRequestDto } from './dto/cats.request.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cat } from './cats.schema';
+import { Model } from 'mongoose';
+import { Injectable, HttpException } from '@nestjs/common';
+
+@Injectable()
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async existsByEmail(email: string): Promise<boolean> {
+    const result = await this.catModel.exists({ email });
+    return result;
+  }
+
+  async create(cat: CatRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat);
+  }
+}
